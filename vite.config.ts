ebfd3c63bfe1396276b273bc82bdfe5b310282f7
Ignacio Jimenez <ignacio.jimenezcuenca@wearejust.co.uk>
2024-09-25 11:58:38 +0100

    copy files

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import { normalizePath } from 'vite'
+import path from 'node:path'
+
+import { viteStaticCopy as copy } from "vite-plugin-static-copy";
+
 import { peerDependencies, name } from "./package.json";
+
+function resolve(relativePath: string) {
+  return normalizePath(path.resolve(__dirname, relativePath));
+}
 
 export default defineConfig({
   build: {
@@ -19,6 +28,9 @@ export default defineConfig({
   plugins: [
     dts({
       // rollupTypes: true
+    }),
+    copy({
+      targets: [{ src: resolve("src/assets"), dest: resolve("dist/assets") }],
     }),
   ]
 });
